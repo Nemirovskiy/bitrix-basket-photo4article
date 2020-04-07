886bcc6875620ca51c68e4daad187c2ddb3fc825
nemirovv <it-nikola@mail.ru>
2020-04-07 10:55:02 +0300

    добавление класса в автозагрузку

--- php_interface/init.php
+++ php_interface/init.php
@@ -1,4 +1,7 @@
 <?
+$arClass = array(
+	"\\Nemin\\Order" => '/local/lib/Nemin/Order.php',
+);
 Bitrix\Main\Loader::registerAutoLoadClasses(null, $arClass);
 $root = Bitrix\Main\Context::getCurrent()->getServer()->getDocumentRoot();
 require $root."/local/php_interface/include/events.php";
